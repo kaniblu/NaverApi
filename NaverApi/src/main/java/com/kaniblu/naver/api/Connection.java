1410b8c54b6e7dad8ead3d2b5bd2cbf279b55033
hanmanhui <hanmanhui@gmail.com>
2015-02-09 17:49:10 +0900

    New class AuthorizedConnection

--- NaverApi/src/main/java/com/kaniblu/naver/api/Connection.java
+++ NaverApi/src/main/java/com/kaniblu/naver/api/Connection.java
@@ -204,19 +204,11 @@ public class Connection
     }
     protected static final Pattern ERR_PATTERN = Pattern.compile("<div class=\"error\" id=\"err_common\">.*?<p>([^<]*?)</p>.*?</div>", Pattern.DOTALL);
 
-    protected String mUsername;
-    protected String mPassword;
     protected KeySet mKeySet;
     protected CookieCollection mCookies = new CookieCollection();
 
     public Connection()
     {
-    }
-
-    public Connection(String username, String password)
-    {
-        mUsername = username;
-        mPassword = password;
     }
 
     protected static class KeySet
@@ -263,41 +255,6 @@ public class Connection
         mKeySet = keySet;
     }
 
-    protected static String getCharCode(String s)
-    {
-        return String.valueOf((char)s.length());
-    }
-
-    protected String getEncryptedCredentials()
-    {
-        RSA rsa = new RSA();
-        String encrypt;
-        rsa.setPublic(mKeySet.eValue, mKeySet.nValue);
-
-        encrypt = rsa.encrypt(getCharCode(mKeySet.sessionKey) + mKeySet.sessionKey + getCharCode(mUsername) + mUsername + getCharCode(mPassword) + mPassword);
-        return encrypt;
-    }
-
-    protected HttpForm generateLoginRequestForm()
-    {
-    	HttpForm formContent = new HttpForm();
-        String encrypted = getEncryptedCredentials();
-
-        if (encrypted == null) {
-            logger.log(Level.SEVERE, "Failed to encrypt credentials.");
-            return null;
-        }
-
-        formContent.put("enctp", "1");
-        formContent.put("encpw", encrypted);
-        formContent.put("encnm", mKeySet.keyName);
-        formContent.put("locale", "en_US");
-        formContent.put("smart_LEVEL", "1");
-        formContent.put("url", "http://www.naver.com");
-
-        return formContent;
-    }
-
     protected String generateCookieHeader()
     {
         String s = "";
@@ -318,25 +275,6 @@ public class Connection
         header.put("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.99 Safari/537.36");
         header.put("Connection", "keep-alive");
         return header;
-    }
-
-    protected HttpHeaders generateLoginRequestHeader()
-    {
-        HttpHeaders header = new HttpHeaders();
-        header.put("Content-Type", "application/x-www-form-urlencoded");
-        header.put("Host", "nid.naver.com");
-
-        return header;
-    }
-
-    public void requestCookies() throws InternalException, ServerException
-    {
-        HttpResult result = requestGet("https://nid.naver.com/nidlogin.login", null, null);
-
-        if (result.statusCode / 100 != 2) {
-            logger.log(Level.SEVERE, "Naver is not available.");
-            throw new InternalException("Naver is not available.");
-        }
     }
 
     public HttpResult requestGet(String url, HttpHeaders header, HttpForm content) throws ServerException
@@ -504,62 +442,5 @@ public class Connection
             return m.group(1);
         else
             return null;
-    }
-
-    protected void requestLogin() throws InternalException, LoginException, ServerException
-    {
-        HttpHeaders header = generateLoginRequestHeader();
-        HttpForm formContent = generateLoginRequestForm();
-
-        if (header == null || formContent == null) {
-            logger.log(Level.SEVERE, "Failed to generate header or form content.");
-            throw new InternalException();
-        }
-
-        HttpResult loginResult = requestPost("https://nid.naver.com/nidlogin.login", header, formContent);
-        if (!loginResult.isStatusOk()) {
-            logger.log(Level.FINE, "Login failed.");
-            throw new InternalException();
-        }
-
-        if (mCookies.contains("NID_SES") && mCookies.contains("NID_AUT")) {
-            logger.log(Level.INFO, "Login was successful.");
-            return;
-        }
-
-        if (loginResult.hasContent()) {
-            String content = loginResult.getContentAsString();
-            String msg = extractErrorMsg(content);
-
-            if (msg == null) {
-                logger.log(Level.SEVERE, "Couldn't find error message in the naver response.");
-                throw new InternalException();
-            } else {
-                logger.log(Level.FINE, "Server returned the following msg: " + msg);
-                throw new LoginException(msg);
-            }
-        } else {
-            logger.log(Level.WARNING, "No content was returned by the server.");
-            throw new InternalException();
-        }
-    }
-
-    public void login() throws InternalException, LoginException, ServerException
-    {
-        if (mUsername == null || mPassword == null)
-            throw new LoginException("Username and password are required.");
-
-        requestKeys();
-        requestLogin();
-    }
-
-    public void setUsername(String username)
-    {
-        mUsername = username;
-    }
-
-    public void setPassword(String password)
-    {
-        mPassword = password;
     }
 }
